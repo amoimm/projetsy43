bab365d79d50194b0d261677bc97e5f578ea50eb
amoimm <v05.axel13@gmail.com>
2026-04-14 17:31:49 +0200

    cm

--- application/app/src/main/java/com/example/application/MainActivity.kt
+++ application/app/src/main/java/com/example/application/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.application.ui.CapaciteScreen
 import com.example.application.ui.PersonalInfoScreen
+import com.example.application.ui.BuildToDoListScreen
 import com.example.application.ui.WelcomeScreen
 import com.example.application.ui.theme.ApplicationTheme
 
@@ -37,13 +38,16 @@ class MainActivity : ComponentActivity() {
                         )
                         "capacite_info" -> CapaciteScreen(
                             modifier = Modifier.padding(innerPadding),
-                            onValidateClick = { currentScreen="HowYouFeelScreen" }
+                            onValidateClick = { currentScreen="HowYouFeel" }
                         )
-                        "HowYouFeelScreen" -> HowYouFeelScreen(
+                        "HowYouFeel" -> HowYouFeelScreen(
+                            modifier = Modifier.padding(innerPadding),
+                            onValidateClick = { currentScreen="Build_ToDo_List" }
+                        )
+                        "Build_ToDo_List" -> BuildToDoListScreen)(
                             modifier = Modifier.padding(innerPadding),
                             onValidateClick = { /* Handle validation */ }
-
-
+                        )
 
 
                     }
